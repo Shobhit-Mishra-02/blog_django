8dbcd7a515f698d18be5d82fe171dfcbe04688d5
Shobhit <shobhitmishra2002@gmail.com>
2021-07-14 08:06:18 +0530

    added the basic pipeline in the blog_tech urls

---
+++ blog_tech/urls.py
@@ -0,0 +1,10 @@
+
+from os import name
+from django.urls import path
+from blog_tech import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog'),
+    path('about/', views.about, name='about'),
+]
--- blog_tech/views.py
+++ blog_tech/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('home page')
+
+def blog(request):
+    return HttpResponse('blog page')
+
+def about(request):
+    return HttpResponse('about page')
