d31220fee5650e969b07389be8f89acb94d6d0c3
Shobhit <shobhitmishra2002@gmail.com>
2021-07-15 08:32:51 +0530

    added the content from the database in blog and home page

--- blog_tech/views.py
+++ blog_tech/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from blog_tech.models import post
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    all_posts = post.objects.all()
+    req_posts = post.objects.extra(where=[f'id>{len(all_posts)-3}'])
+
+    return render(request, 'home.html',{'content':req_posts})
 
 def blog(request):
-    return render(request, 'blog.html')
+    req_post = post.objects.filter(status='main')
+    return render(request, 'blog.html', {'content':req_post})
 
 def about(request):
     return render(request, 'about.html')
 
-def postview(request):
-    return render(request, 'postview.html')
+def postview(request, id):
+    req_post = post.objects.filter(id = id)
+    return render(request, 'postview.html',{'content':req_post})
 
 def search(request):
     return render(request, 'search.html')
